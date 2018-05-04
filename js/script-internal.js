9bc37ee8e9336d3d02cd6a28713c3ad8b359bf6e
Verta-master <vertyagina@gmail.com>
2018-05-04 14:04:07 +0600

    Logo

--- js/script-internal.js
+++ js/script-internal.js
@@ -3,4 +3,10 @@ $('.menu__btn').click(function() {
   $('.menu').toggleClass('menu--opened');
   $('.logo').toggleClass('logo--dark');
   $('body').toggleClass('noscroll');
+  
+  if ($('.logo').hasClass('logo--dark')) {
+    $('.logo img').attr('src', 'img/logo-white.svg');
+  } else {
+    $('.logo img').attr('src', 'img/logo.svg');
+  }
 })
